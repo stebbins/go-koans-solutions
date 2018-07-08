7880e7d0e96708b88a73f878c991555582fa5ad9
Harry Stebbins <hcstebbins@gmail.com>
2018-07-08 12:06:15 -0400

    Solve koans

--- about_panics.go
+++ about_panics.go
@@ -4,11 +4,10 @@ func divideFourBy(i int) int {
 	return 4 / i
 }
 
-const __divisor__ = 0
+const div = 0
 
 func aboutPanics() {
-	assert(__delete_me__) // panics are exceptional errors at runtime
 
-	n := divideFourBy(__divisor__)
+	n := divideFourBy(2)
 	assert(n == 2) // panics are exceptional errors at runtime
 }
